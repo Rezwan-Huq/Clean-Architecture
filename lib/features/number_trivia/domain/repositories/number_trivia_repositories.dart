ce90ebed1413b0280f5cc2d1b71c768acff5c65b
Rezwan <rezwan.huq@cellosco.pe>
2022-12-21 18:17:28 +0600

    change

--- lib/features/number_trivia/domain/repositories/number_trivia_repositories.dart
+++ lib/features/number_trivia/domain/repositories/number_trivia_repositories.dart
@@ -1,6 +1,8 @@
+import 'package:clean_architecture/core/error/failures.dart';
+import 'package:clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class NumberTriviaRepository {
-  Future <Either>getConcreteNumberTrivia(int Number);
-  getRandomNumberTrivia(int Number);
+  Future <Either<Failure, Numbertrivia>>getConcreteNumberTrivia(int Number);
+  Future<Either<Failure,Numbertrivia>>getRandomNumberTrivia(int Number);
 }
---
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -0,0 +1,27 @@
+import 'package:clean_architecture/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture/features/number_trivia/domain/repositories/number_trivia_repositories.dart';
+import 'package:clean_architecture/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class MockNumberTriviaRepository extends Mock
+    implements NumberTriviaRepository {}
+
+void main() {
+  GetConcreteNumberTrivia usecase;
+  MockNumberTriviaRepository mockNumberTriviaRepository;
+
+  setUp(() {
+    mockNumberTriviaRepository = MockNumberTriviaRepository();
+    usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
+  });
+
+final tNumber = 1;
+final tNumberTrivia = Numbertrivia(text: 'test', number: tNumber);
+
+// test('should get trivia for the number from the reporsitory', body)
+}
+
+
+
+
